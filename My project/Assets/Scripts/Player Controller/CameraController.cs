c8f94b2685abe2e98925220df36e5887a6c9c7d4
Matthew Wierenga <matt.wierenga@outlook.com>
2023-10-11 08:42:46 +0200

    Updated camera pan

--- My project/Assets/Scripts/Player Controller/CameraController.cs
+++ My project/Assets/Scripts/Player Controller/CameraController.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float panSpeed = 10f; // Adjust this value for pan speed.
+    public float panSpeed = 10f;
+    public Vector2 minXMax = new Vector2(0, 10); // Define the minimum and maximum X-axis values.
+    public Vector2 minYMax = new Vector2(0, 10); // Define the minimum and maximum Y-axis values.
+
     private Vector3 lastMousePosition;
 
     void Update()
     {
-        // Camera panning with the mouse.
         if (Input.GetMouseButtonDown(0))
         {
             lastMousePosition = Input.mousePosition;
         }
+
         if (Input.GetMouseButton(0))
         {
             Vector3 offset = lastMousePosition - Input.mousePosition;
-            transform.Translate(offset * panSpeed * Time.deltaTime);
+            Vector3 newPosition = transform.position + new Vector3(offset.x * panSpeed * Time.deltaTime, offset.y * panSpeed * Time.deltaTime, 0);
+
+            // Clamp the camera's X and Y positions within the specified ranges.
+            newPosition.x = Mathf.Clamp(newPosition.x, minXMax.x, minXMax.y);
+            newPosition.y = Mathf.Clamp(newPosition.y, minYMax.x, minYMax.y);
+
+            transform.position = newPosition;
             lastMousePosition = Input.mousePosition;
         }
     }
